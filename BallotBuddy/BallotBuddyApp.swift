8f42c132e3095edfb41e0e7eed68252532ea5362
aidenc08 <aidenchavda@gmail.com>
2024-08-17 09:16:22 -0700

    Initial Commit

---
+++ BallotBuddy/BallotBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallotBuddyApp.swift
+//  BallotBuddy
+//
+//  Created by Aiden Chavda on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BallotBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
